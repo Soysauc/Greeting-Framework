40ec67119b82d25a5b08833a6077c6a5537f11ee
Jeremy M Rivera <fangman09@gmail.com>
2022-11-16 21:26:32 -0500

    first commit

---
+++ Greetr.js
@@ -0,0 +1,11 @@
+(function (global, $) {
+  var Greetr = function (firstname, lastname, language) {
+    return new Greetr.init(firstname, lastname, language);
+  };
+  Greetr.init = function (firstname, lastname, language) {
+    var self = this;
+    self.firstname = firstname || "";
+    self.lastname = lastname || "";
+    self.language = language || "en";
+  };
+})(window, jQuery);
